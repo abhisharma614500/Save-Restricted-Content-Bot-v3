b5a1e13d85dca71649eb13ea755df18e2fadcb70
abhisharma614500 <abhisharma614500@gmail.com>
2026-09-14 09:46:27 +0530

    Update configuration settings in config.py

--- config.py
+++ config.py
@@ -23,7 +23,7 @@ YTUB_COOKIES = """
 API_ID       = os.getenv("API_ID", "")
 API_HASH     = os.getenv("API_HASH", "")
 BOT_TOKEN    = os.getenv("BOT_TOKEN", "")
-MONGO_DB     = os.getenv("MONGO_DB", "")
+MONGO_DB = "mongodb://localhost:27017"
 DB_NAME      = os.getenv("DB_NAME", "telegram_downloader")
 
 # ─── OWNER / CONTROL SETTINGS ───────────────────────────────────────────────────
